c6b09b1f98a6a3a316fc42d9992bd92181ac25f5
FeArZ0z <max.spiesmaier@aon.at>
2021-05-03 15:54:43 +0200

    Opens Window with "OK" Button

---
+++ main.py
@@ -0,0 +1,12 @@
+import PySimpleGUI as sg
+
+layout = [[sg.Text("Check your License")], [sg.Button("OK")]]
+#margins = (200,150)
+window = sg.Window("License Check", layout, margins=(200,150))
+
+while True:
+    event, values = window.read()
+    if event =="OK" or event == sg.WIN_CLOSED:
+        break
+
+window.close()
