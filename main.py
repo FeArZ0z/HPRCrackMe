e64e7cc8c7854da747ec616a35f01e4b8b497ccc
FeArZ0z <max.spiesmaier@aon.at>
2021-05-04 16:24:14 +0200

    finished?

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import PySimpleGUI as sg
 import traceback
-
+import base64
 # input fields and Layout for the Window
 layout = [
     [sg.Text('Check your License')], 
@@ -25,7 +25,17 @@ try:
                 #TODO: implement License Key validation
                 # String builder from input encode in b64 and compare
                 # if not the same: Contacting Server, if the same Success message
-                sg.Popup('Contacting Server, this may take a while.')
+                joinedWords = values[0] + ',' + values[1] + ',' + values[2]
+                encodedWords = base64.b64encode(joinedWords.encode("utf-8"))
+                #sg.Popup(encodedWords)
+                licenseBase = "johndoe,rottenTomatoes24,1324-DEAD-BEEF-5678"
+                validLicense = base64.b64encode(licenseBase.encode("utf-8"))
+                # Check if input == license
+                if (encodedWords == validLicense):#
+                    sg.Popup('You Hacked the World')
+                else:
+                    sg.Popup('Contacting Server, this may take a while.')
+                    #sg.Popup(encodedWords + '   ' + validLicense)
         if event == 'Cancel':
             break
     window.close()
